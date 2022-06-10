18ca911192b7e1836a3a225a7d6da9b2fea3398c
AAbutahir <73829548+AAbutahir@users.noreply.github.com>
2022-06-10 17:49:00 +0530

    Update Header.js

--- src/Header.js
+++ src/Header.js
@@ -7,8 +7,20 @@ function Header() {
             <div className="nav-bar">
                 <div className="logo-container">
                     <img src={logo} className="logo-img"  />
-                </div>    
+                </div>  
+                <ul className='nav-menu'>
+                    <li><a>Home</a></li>
+                    <li><a>About</a></li>
+                    <li><a>Blog</a></li>
+                    <li><a>Contact</a></li>
+                </ul>  
             </div>
+            <div className='header-container'>
+                <div className='header-content'>
+                    Let's Do together
+                </div>
+            </div>
+            
         </div>
     )
 }
