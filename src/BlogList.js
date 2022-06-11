dcaf49bdb7544d8f4f85d740a6cbb0eeaa850593
AAbutahir <73829548+AAbutahir@users.noreply.github.com>
2022-06-11 18:34:32 +0530

    Update BlogList.js

--- src/BlogList.js
+++ src/BlogList.js
@@ -1,3 +1,4 @@
+import { Link } from 'react-router-dom';
 import './BlogList.css'
 
 
@@ -5,32 +6,39 @@ const BlogList = (props) => {
     console.log("DATA: ", props)
     
     return(
-        <div className="blog-container">
-            {props.blogData.map(list => (
-                <div className="blog-thumbnail col-4" key={list.id}>
-                    <div className="img-wrapper">
-                        <img src={list.imgurl} />
-                    </div>
-                    <div className='blog-content'>
-                        <div className='blog-content-header'>
-                            {list.title}
-                        </div>
-                        <div className='blog-content-description'>
-                            {list.description}
-                        </div>
-                        <div className='blog-content-author'>
-                            <div className='author-name'>
-                                {list.author.name}
+        <div>
+            <Link to='/addBlog' >
+                <button type='button' className='button'>+ Add New Blog</button>
+            </Link>
+            <div className="blog-container">
+                {props.blogData.map(list => (
+                    <Link to={`/singleBlog/${list.id}`} >
+                        <div className="blog-thumbnail col-4" key={list.id} >
+                            <div className="img-wrapper">
+                                <img src={list.imgurl} />
                             </div>
-                            <div className='blog-date'>
-                                {list.author.date}
+                            <div className='blog-content'>
+                                <div className='blog-content-header'>
+                                    {list.title}
+                                </div>
+                                <div className='blog-content-description'>
+                                    {list.description}
+                                </div>
+                                <div className='blog-content-author'>
+                                    <div className='author-name'>
+                                        {list.author.name}
+                                    </div>
+                                    <div className='blog-date'>
+                                        {list.date}
+                                    </div>
+                                </div>
                             </div>
                         </div>
-                    </div>
-                </div>
-            ))}
-            
-            
+                    </Link>
+                ))}
+                
+                
+            </div>
         </div>
     )
 }
